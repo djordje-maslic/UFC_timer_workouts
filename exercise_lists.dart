83638cd0aa3023d19a883514dff7a252cc696a5e
Djordje Maslic <60461885+djordje-maslic@users.noreply.github.com>
2022-03-10 08:40:03 +0100

    Update exercise_lists.dart

--- exercise_lists.dart
+++ exercise_lists.dart
@@ -1,12 +1,202 @@
 [
-  {"name":"fight1","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/anderson-silva-vitor-belort-ufc.jpg?raw=true","id":1},
-  {"name":"fight2","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/icon-512.png?raw=true","id":2},
-  {"name":"fight3","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/roby_vs_mcdonald.jpg?raw=true","id":3},
-  {"name":"fight4","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/anderson-silva-vitor-belort-ufc.jpg?raw=true","id":4},
-  {"name":"fight5","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/icon-512.png?raw=true","id":5},
-  {"name":"fight6","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/roby_vs_mcdonald.jpg?raw=true","id":6},
-{"name":"fight7","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/anderson-silva-vitor-belort-ufc.jpg?raw=true","id":7},
-  {"name":"fight8","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/icon-512.png?raw=true","id":8},
-  {"name":"fight9","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/roby_vs_mcdonald.jpg?raw=true","id":9},
-  {"name":"fight10","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/anderson-silva-vitor-belort-ufc.jpg?raw=true","id":10}
+  {"name":"fight1 Khabib","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/anderson-silva-vitor-belort-ufc.jpg?raw=true","id":0,
+  "big_ex_list":[{"name":"Ready", "duration":10, "id":"0"},
+                 {"name":"Round1", "duration":300, "id":"1"},
+                 {"name":"Rest", "duration":60, "id":"2"},
+                 {"name":"Round2", "duration":300, "id":"3"},
+                 {"name":"Rest", "duration":60, "id":"4"},
+                 {"name":"Round3", "duration":300, "id":"5"},
+                 {"name":"Rest", "duration":60, "id":"6"},
+                 {"name":"Round4", "duration":300, "id":"7"},
+                 {"name":"Rest", "duration":60, "id":"8"},
+                 {"name":"Round5", "duration":300, "id":"7"}
+                ], 
+   "little_ex_list":[
+                 {"name":"Punch", "duration":3, "id":0, "power":100},
+                 {"name":"Kick", "duration":3, "id":1, "power": 70},
+                 {"name":"Shut", "duration":3, "id":2, "power": 50},
+                 {"name":"Sproll", "duration":3, "id":3, "power": 60},
+                 {"name":"Wressl", "duration":3, "id":4, "power": 30}
+                 
+   ]},
+  {"name":"fight2 Usman","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/icon-512.png?raw=true","id":1,
+  "big_ex_list":[{"name":"Ready", "duration":10, "id":"0"},
+                 {"name":"Round1", "duration":300, "id":"1"},
+                 {"name":"Rest", "duration":60, "id":"2"},
+                 {"name":"Round2", "duration":300, "id":"3"},
+                 {"name":"Rest", "duration":60, "id":"4"},
+                 {"name":"Round3", "duration":300, "id":"5"},
+                 {"name":"Rest", "duration":60, "id":"6"},
+                 {"name":"Round4", "duration":300, "id":"7"},
+                 {"name":"Rest", "duration":60, "id":"8"},
+                 {"name":"Round5", "duration":300, "id":"7"}
+                ], 
+   "little_ex_list":[
+                 {"name":"Punch", "duration":3, "id":0, "power":100},
+                 {"name":"Kick", "duration":3, "id":1, "power": 70},
+                 {"name":"Shut", "duration":3, "id":2, "power": 50},
+                 {"name":"Sproll", "duration":3, "id":3, "power": 60},
+                 {"name":"Wressl", "duration":3, "id":4, "power": 30}
+                 
+   ]},
+  {"name":"fight3 JSP","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/roby_vs_mcdonald.jpg?raw=true","id":2,
+  "big_ex_list":[{"name":"Ready", "duration":10, "id":"0"},
+                 {"name":"Round1", "duration":300, "id":"1"},
+                 {"name":"Rest", "duration":60, "id":"2"},
+                 {"name":"Round2", "duration":300, "id":"3"},
+                 {"name":"Rest", "duration":60, "id":"4"},
+                 {"name":"Round3", "duration":300, "id":"5"},
+                 {"name":"Rest", "duration":60, "id":"6"},
+                 {"name":"Round4", "duration":300, "id":"7"},
+                 {"name":"Rest", "duration":60, "id":"8"},
+                 {"name":"Round5", "duration":300, "id":"7"}
+                ], 
+   "little_ex_list":[
+                 {"name":"Punch", "duration":3, "id":0, "power":100},
+                 {"name":"Kick", "duration":3, "id":1, "power": 70},
+                 {"name":"Shut", "duration":3, "id":2, "power": 50},
+                 {"name":"Sproll", "duration":3, "id":3, "power": 60},
+                 {"name":"Wressl", "duration":3, "id":4, "power": 30}
+                 
+   ]},
+  {"name":"fight4 MCgregor","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/anderson-silva-vitor-belort-ufc.jpg?raw=true","id":3,
+  "big_ex_list":[{"name":"Ready", "duration":10, "id":"0"},
+                 {"name":"Round1", "duration":300, "id":"1"},
+                 {"name":"Rest", "duration":60, "id":"2"},
+                 {"name":"Round2", "duration":300, "id":"3"},
+                 {"name":"Rest", "duration":60, "id":"4"},
+                 {"name":"Round3", "duration":300, "id":"5"},
+                 {"name":"Rest", "duration":60, "id":"6"},
+                 {"name":"Round4", "duration":300, "id":"7"},
+                 {"name":"Rest", "duration":60, "id":"8"},
+                 {"name":"Round5", "duration":300, "id":"7"}
+                ], 
+   "little_ex_list":[
+                 {"name":"Punch", "duration":3, "id":0, "power":100},
+                 {"name":"Kick", "duration":3, "id":1, "power": 70},
+                 {"name":"Shut", "duration":3, "id":2, "power": 50},
+                 {"name":"Sproll", "duration":3, "id":3, "power": 60},
+                 {"name":"Wressl", "duration":3, "id":4, "power": 30}
+                 
+   ]},
+  {"name":"fight5 RDE","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/icon-512.png?raw=true","id":4,
+  "big_ex_list":[{"name":"Ready", "duration":10, "id":"0"},
+                 {"name":"Round1", "duration":300, "id":"1"},
+                 {"name":"Rest", "duration":60, "id":"2"},
+                 {"name":"Round2", "duration":300, "id":"3"},
+                 {"name":"Rest", "duration":60, "id":"4"},
+                 {"name":"Round3", "duration":300, "id":"5"},
+                 {"name":"Rest", "duration":60, "id":"6"},
+                 {"name":"Round4", "duration":300, "id":"7"},
+                 {"name":"Rest", "duration":60, "id":"8"},
+                 {"name":"Round5", "duration":300, "id":"7"}
+                ], 
+   "little_ex_list":[
+                 {"name":"Punch", "duration":3, "id":0, "power":100},
+                 {"name":"Kick", "duration":3, "id":1, "power": 70},
+                 {"name":"Shut", "duration":3, "id":2, "power": 50},
+                 {"name":"Sproll", "duration":3, "id":3, "power": 60},
+                 {"name":"Wressl", "duration":3, "id":4, "power": 30}
+                 
+   ]},
+  {"name":"fight6 Anderson","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/roby_vs_mcdonald.jpg?raw=true","id":5,
+  "big_ex_list":[{"name":"Ready", "duration":10, "id":"0"},
+                 {"name":"Round1", "duration":300, "id":"1"},
+                 {"name":"Rest", "duration":60, "id":"2"},
+                 {"name":"Round2", "duration":300, "id":"3"},
+                 {"name":"Rest", "duration":60, "id":"4"},
+                 {"name":"Round3", "duration":300, "id":"5"},
+                 {"name":"Rest", "duration":60, "id":"6"},
+                 {"name":"Round4", "duration":300, "id":"7"},
+                 {"name":"Rest", "duration":60, "id":"8"},
+                 {"name":"Round5", "duration":300, "id":"7"}
+                ], 
+   "little_ex_list":[
+                 {"name":"Punch", "duration":3, "id":0, "power":100},
+                 {"name":"Kick", "duration":3, "id":1, "power": 70},
+                 {"name":"Shut", "duration":3, "id":2, "power": 50},
+                 {"name":"Sproll", "duration":3, "id":3, "power": 60},
+                 {"name":"Wressl", "duration":3, "id":4, "power": 30}
+                 
+   ]},
+{"name":"fight7 yyy","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/anderson-silva-vitor-belort-ufc.jpg?raw=true","id":6,
+  "big_ex_list":[{"name":"Ready", "duration":10, "id":"0"},
+                 {"name":"Round1", "duration":300, "id":"1"},
+                 {"name":"Rest", "duration":60, "id":"2"},
+                 {"name":"Round2", "duration":300, "id":"3"},
+                 {"name":"Rest", "duration":60, "id":"4"},
+                 {"name":"Round3", "duration":300, "id":"5"},
+                 {"name":"Rest", "duration":60, "id":"6"},
+                 {"name":"Round4", "duration":300, "id":"7"},
+                 {"name":"Rest", "duration":60, "id":"8"},
+                 {"name":"Round5", "duration":300, "id":"7"}
+                ], 
+   "little_ex_list":[
+                 {"name":"Punch", "duration":3, "id":0, "power":100},
+                 {"name":"Kick", "duration":3, "id":1, "power": 70},
+                 {"name":"Shut", "duration":3, "id":2, "power": 50},
+                 {"name":"Sproll", "duration":3, "id":3, "power": 60},
+                 {"name":"Wressl", "duration":3, "id":4, "power": 30}
+                 
+   ]},
+  {"name":"fight8","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/icon-512.png?raw=true","id":7,
+  "big_ex_list":[{"name":"Ready", "duration":10, "id":"0"},
+                 {"name":"Round1", "duration":300, "id":"1"},
+                 {"name":"Rest", "duration":60, "id":"2"},
+                 {"name":"Round2", "duration":300, "id":"3"},
+                 {"name":"Rest", "duration":60, "id":"4"},
+                 {"name":"Round3", "duration":300, "id":"5"},
+                 {"name":"Rest", "duration":60, "id":"6"},
+                 {"name":"Round4", "duration":300, "id":"7"},
+                 {"name":"Rest", "duration":60, "id":"8"},
+                 {"name":"Round5", "duration":300, "id":"7"}
+                ], 
+   "little_ex_list":[
+                 {"name":"Punch", "duration":3, "id":0, "power":100},
+                 {"name":"Kick", "duration":3, "id":1, "power": 70},
+                 {"name":"Shut", "duration":3, "id":2, "power": 50},
+                 {"name":"Sproll", "duration":3, "id":3, "power": 60},
+                 {"name":"Wressl", "duration":3, "id":4, "power": 30}
+                 
+   ]},
+  {"name":"fight9","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/roby_vs_mcdonald.jpg?raw=true","id":8,
+  "big_ex_list":[{"name":"Ready", "duration":10, "id":"0"},
+                 {"name":"Round1", "duration":300, "id":"1"},
+                 {"name":"Rest", "duration":60, "id":"2"},
+                 {"name":"Round2", "duration":300, "id":"3"},
+                 {"name":"Rest", "duration":60, "id":"4"},
+                 {"name":"Round3", "duration":300, "id":"5"},
+                 {"name":"Rest", "duration":60, "id":"6"},
+                 {"name":"Round4", "duration":300, "id":"7"},
+                 {"name":"Rest", "duration":60, "id":"8"},
+                 {"name":"Round5", "duration":300, "id":"7"}
+                ], 
+   "little_ex_list":[
+                 {"name":"Punch", "duration":3, "id":0, "power":100},
+                 {"name":"Kick", "duration":3, "id":1, "power": 70},
+                 {"name":"Shut", "duration":3, "id":2, "power": 50},
+                 {"name":"Sproll", "duration":3, "id":3, "power": 60},
+                 {"name":"Wressl", "duration":3, "id":4, "power": 30}
+                 
+   ]},
+  {"name":"fight10","image":"https://github.com/djordje-maslic/UFC_timer_workouts/blob/main/anderson-silva-vitor-belort-ufc.jpg?raw=true","id":9,
+  "big_ex_list":[{"name":"Ready", "duration":10, "id":"0"},
+                 {"name":"Round1", "duration":300, "id":"1"},
+                 {"name":"Rest", "duration":60, "id":"2"},
+                 {"name":"Round2", "duration":300, "id":"3"},
+                 {"name":"Rest", "duration":60, "id":"4"},
+                 {"name":"Round3", "duration":300, "id":"5"},
+                 {"name":"Rest", "duration":60, "id":"6"},
+                 {"name":"Round4", "duration":300, "id":"7"},
+                 {"name":"Rest", "duration":60, "id":"8"},
+                 {"name":"Round5", "duration":300, "id":"7"}
+                ], 
+   "little_ex_list":[
+                 {"name":"Punch", "duration":3, "id":0, "power":100},
+                 {"name":"Kick", "duration":3, "id":1, "power": 70},
+                 {"name":"Shut", "duration":3, "id":2, "power": 50},
+                 {"name":"Sproll", "duration":3, "id":3, "power": 60},
+                 {"name":"Wressl", "duration":3, "id":4, "power": 30}
+                 
+   ]}
 ]
